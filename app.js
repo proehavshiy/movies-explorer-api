e19f33c41a3bceed235c7377232a996d820e99c3
Egor Kurkin <proehavshiynapravo@yandex.ru>
2021-09-09 18:10:11 +0300

    feat: initial settings

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+const { PORT = 3000 } = process.env;
+
+app.listen(PORT, () => {
+  // Если всё работает, консоль покажет, какой порт приложение слушает
+  console.log(`App listening on port ${PORT}`)
+})
