b5ef7f39ac0a560c0b557053c870b7e346faf087
Egor Kurkin <proehavshiynapravo@yandex.ru>
2021-09-09 18:24:46 +0300

    feat: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  rules: {
+    // 'no-underscore-dangle': off,
+    'no-underscore-dangle': ['error', { allow: ['_id'] }],
+  },
+};
